942fa5b0be44cc95a74d10f5d53ac942b28dcf02
Juan Bedoya <extiba@gmail.com>
2019-05-27 16:55:05 -0500

    practice removed

--- weekly_expenditure/src/components/Expenditure.js
+++ weekly_expenditure/src/components/Expenditure.js
@@ -4,7 +4,9 @@ import React, { Component } from "react";
 class Expenditure extends Component {
     render() {
         return(
-            
+            <div>
+                
+            </div>
         );
     }
 }
