7298c379b191e509368ac429e2698609ef64e288
Juan Bedoya <extiba@gmail.com>
2019-05-28 00:29:24 -0500

    new react practice initialized

---
+++ veterinary_patients/src/App.js
@@ -0,0 +1,14 @@
+/* Modules */
+import React from 'react';
+
+function App() {
+  return (
+   <div>
+     <p>
+       Hello World
+     </p>
+   </div>
+  );
+}
+
+export default App;
---
+++ veterinary_patients/src/index.js
@@ -0,0 +1,12 @@
+/* Modules */
+import React from "react";
+import ReactDOM from "react-dom";
+
+/* Styles */
+import "./index.css";
+import "./bootstrap.min.css";
+
+/* Components */
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
